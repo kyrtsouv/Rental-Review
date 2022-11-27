3934140520ae157097b1396e480158e33a7a34b7
kyrtsouv <kyrtsouv@csd.auth.gr>
2022-11-27 18:37:04 +0200

    Basic Implemantation of Review, Rental and a Main

---
+++ src/api/Main.java
@@ -0,0 +1,16 @@
+package api;
+
+/**
+ * This class can be deleted
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Rental r = new Rental("Flat in Kifisia", "Kanellopoulou 21", "Thessaloniki", "54248", "Big house", "Flat",
+                "Viktor Kyrtsoudis");
+
+        r.addReview(new Review("John Wick", "Amazing house with all amenities ", 5));
+        r.print();
+    }
+
+}
