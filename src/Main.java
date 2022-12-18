e2088aeaa1eaccc9e8a2436391402968be9a8424
elenianagn <elenianagn@csd.auth.gr>
2022-12-18 18:14:07 +0200

    Σχολια στη main για επόμενα steps

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import api.*;
 //ΓΕΝΙΚΑ μας εχει μεινει στο κομματι API: database,renter,tenant,research
 //Μετά θα δοκιμάσουμε με αυτό το πρόγραμμα που μας έχει δώσει ο Τσουμάκας για να διοθρώσουμε το κώδικα αν χρειάζεται και μετά έχουμε tests & GUI
 //ΕΠΙΣΗΣ πρεπει να δούμε το controller για να μην υπάρχει κάποιο θέμα
+
+
 public class Main {
 
     public static void main(String[] args) {
