74c62a5b6c750b9b0b882cde1b4f537f30841934
elenianagn <elenianagn@csd.auth.gr>
2022-12-21 13:53:49 +0200

    Search still needs to be fixed

--- src/gui/Search.java
+++ src/gui/Search.java
@@ -1,81 +1,80 @@
 package gui;
 
-
+import java.io.*;
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.text.View;
+import java.awt.*;
+import java.util.stream.Collectors;
+import java.util.List;
 
-public class Search extends JPanel {
+public class Search extends JFrame implements DocumentListener {
     //θα πρεπει να εχουμε ενα απλο search bar + φιλτραρισμα στοιχειων για εμφανιση συγκεκριμενου κομματιου
     private View view;
+    private static final long serialVersionUID = -1662279563193298340L;
+    private JList<String> list;
+    private List data = new List<>();
 
+    private DefaultListModel<String> model;
+    private JTextField searchField;
 
-    public Search(final View view, boolean temp){
-        setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
-        this.view=new view;
+    public Search(){
+        //setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
+        //this.view=new view;
+        super("test");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        setLayout(new BorderLayout());
+
+        searchField = new JTextField();
+        searchField.getDocument().addDocumentListener(this);
+        add(searchField, BorderLayout.PAGE_START);
+
+        createData();
+
+        list = new JList<>(model = new DefaultListModel<>());
+        //data.forEach(model::addElement);
+        for(Object temp:data) model.addElement((String) temp);
+        add(new JScrollPane(list), BorderLayout.CENTER);
+
+        setSize(500, 500);
+        setLocationByPlatform(true);
+    }
+    private void createData() {
+        for (int i = 0; i < 1000; i++) {
+            String s = "String: " + i + ".";
+            data.add(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Search example = new Search();
+            example.setVisible(true);
+        });
     }
 
 
 
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        search();
+    }
 
-    /*<div class="topnav">
-      <a class="active" href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#contact">Contact</a>
-      <input type="text" placeholder="Search..">
-    </div>
-        .topnav {
-            overflow: hidden;
-            background-color: #e9e9e9;
-        }
-
-        /* Style the links inside the navigation bar */
-        /*.topnav a {
-            float: left;
-            display: block;
-            color: black;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-            font-size: 17px;
-        }
-
-        /* Change the color of links on hover */
-    /*.topnav a:hover {
-            background-color: #ddd;
-            color: black;
-        }
-
-        /* Style the "active" element to highlight the current page */
-    /*.topnav a.active {
-            background-color: #2196F3;
-            color: white;
-        }
-    */
-
-        /*.topnav input[type=text] {
-            float: right;
-            padding: 6px;
-            border: none;
-            margin-top: 8px;
-            margin-right: 16px;
-            font-size: 17px;
-        }
-    */
-
-      /*  @media screen and (max-width: 600px) {
-      .topnav a, .topnav input[type=text] {
-                float: none;
-                display: block;
-                text-align: left;
-                width: 100%;
-                margin: 0;
-                padding: 14px;
-            }
-      .topnav input[type=text] {
-                border: 1px solid #ccc;
-            }
-        }
-    */
-
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        search();
+    }
+    private void search() {
+        Collectors Collectors = null;
+        List<String> filtered = (List<String>) data.stream().filter(s -> s.toString().toLowerCase().contains(searchField.getText().toLowerCase())).collect(Collectors.toList());
+        model.removeAllElements();
+        for (String temp :filtered) model.addElement(temp);
+    }
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        search();
+    }
 }
+
