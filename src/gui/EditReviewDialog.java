d0f95c4fa0f584219e2df44ea2cbf165b186611d
kyrtsouv <kyrtsouv@csd.auth.gr>
2023-12-20 18:57:52 +0200

    Delete

--- src/gui/EditReviewDialog.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-public class EditReviewDialog extends AddReviewDialog {
-
-    public EditReviewDialog(java.awt.Frame parent, boolean modal) {
-        super(parent, modal);
-    }
-
-}
